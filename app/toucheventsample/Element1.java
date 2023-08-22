2615ffe5f3090eeedd47cd22850d1ef4fcd7dc55
imbakan <131257516+imbakan@users.noreply.github.com>
2023-08-22 13:41:28 +0800

    initial commit

---
+++ app/toucheventsample/Element1.java
@@ -0,0 +1,36 @@
+package balikbayan.box.toucheventsample;
+
+public class Element1 {
+    private float x, y;
+    private int id;
+
+    public Element1(float x, float y, int id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
