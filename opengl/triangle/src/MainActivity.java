3cf34b392f41e0e01490e21f3bed282c1f28a356
imbakan <131257516+imbakan@users.noreply.github.com>
2024-09-26 13:40:49 +0800

    initial commit

---
+++ opengl/triangle/src/MainActivity.java
@@ -0,0 +1,18 @@
+package balikbayan.box.triangle2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView1 view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new SurfaceView1(this);
+        setContentView(view);
+    }
+}
