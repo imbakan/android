7f065ccd5e3d76e2e93909f4486a24ac1aad3e54
imbakan <131257516+imbakan@users.noreply.github.com>
2024-10-19 18:33:38 +0800

    initial commit

--- app/opendialogbox/src/MainActivity.java
+++ app/opendialogbox/src/MainActivity.java
@@ -1,1 +1,36 @@
 
+package balikbayan.box.opendialogbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+  
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.mnuOpen)
+            onFileOpen();
+        else
+            return super.onOptionsItemSelected(item);
+
+        return true;
+    }
+
+}
