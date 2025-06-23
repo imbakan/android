1de4c6ac5f5e0b3fa7ac9642b535ef89922ab691
imbakan <131257516+imbakan@users.noreply.github.com>
2025-06-23 08:22:54 +0800

    initial commit

--- app/clientlan/src/Client.java
+++ app/clientlan/src/Client.java
@@ -1,4 +1,4 @@
-package balikbayan.box.clientlan;
+package balikbayan.box.client_lan;
 
 import android.os.Handler;
 import android.os.Message;
@@ -22,64 +22,67 @@ public class Client implements Runnable {
 
     private final int BUFFER_SIZE = 32768;
 
-    private final int NEED_DATA      = 3101;
-    private final int SET_ORDER      = 3102;
-    private final int GET_INTEGER    = 3103;
-    private final int GET_LONG       = 3104;
-    private final int GET_STRING     = 3105;
+    private final int NEED_DATA      = 3001;
+    private final int SET_ORDER      = 3002;
+    private final int GET_INTEGER    = 3003;
+    private final int GET_LONG       = 3004;
+    private final int GET_STRING     = 3005;
 
-    private final int ATTRIBUTES         = 3201;
-    private final int STRINGS            = 3202;
-    private final int COLLECT_STRING     = 3203;
-    private final int COLLECT_ATTRIBUTE  = 3204;
+    private final int ATTRIBUTES         = 4001;
+    private final int STRINGS            = 4002;
+    private final int COLLECT_STRING     = 4003;
+    private final int COLLECT_ATTRIBUTE  = 4004;
 
-    private final int JOIN               = 3301;
-    private final int LEAVE              = 3302;
-    private final int CLIENT_RUNNING     = 3303;
-    private final int CONFIRM            = 3304;
-    private final int ID_RECEIVED        = 3305;
-    private final int RUN_ONCE           = 3306;
-    private final int FORWARD            = 3307;
+    private final int FIRST_RUN      = 5001;
+    private final int CONNECTED      = 5002;
+    private final int JOIN           = 5003;
+    private final int LEAVE          = 5004;
+    private final int FORWARD        = 5005;
+    private final int RESEND         = 5006;
+    private final int CLIENT_RUNNING = 5007;
 
-    private final int REPLY_DEVICE       = 3401;
-    private final int REPLY_DEVICES      = 3402;
-    private final int REPLY_DRIVE        = 3403;
+    private final int REQUEST_DRIVE      = 6001;
+    private final int REQUEST_DIRECTORY  = 6002;
+    private final int REQUEST_FILE       = 6003;
+    private final int REQUEST_CONTENT    = 6004;
 
-    private final int REQUEST_DRIVE      = 3501;
-    private final int REQUEST_DIRECTORY  = 3502;
-    private final int REQUEST_FILE       = 3503;
-    private final int REQUEST_CONTENT    = 3504;
+    private final int REPLY_DEVICE       = 7001;
+    private final int REPLY_DRIVE        = 7002;
+    private final int REPLY_DIRECTORY    = 7003;
+    private final int REPLY_FILE         = 7004;
+    private final int REPLY_CONTENT_1    = 7005;
+    private final int REPLY_CONTENT_2    = 7006;
+    private final int REPLY_CONTENT_3    = 7007;
 
-    public static final int DEPOPULATE_BY_DEVICE  = 3601;
-    public static final int POPULATE_WITH_DEVICE  = 3602;
-    public static final int POPULATE_WITH_DEVICES = 3603;
-    private final int POPULATE_WITH_DRIVES        = 3604;
+    private final int ENUMERATE_DRIVES       = 8001;
+    private final int ENUMERATE_DIRECTORIES  = 8002;
+    private final int ENUMERATE_FILES        = 8003;
+    private final int ENUMERATE_CONTENTS     = 8004;
 
-    private final int ENUMERATE_DRIVES = 3701;
+    public static final int DEPOPULATE_BY_DEVICE = 9001;
+    public static final int POPULATE_WITH_DEVICE = 9002;
+    private final int POPULATE_WITH_DRIVES       = 9003;
+    private final int POPULATE_WITH_DIRECTORIES  = 9004;
+    private final int POPULATE_WITH_FILES        = 9005;
+    private final int POPULATE_WITH_CONTENT_1    = 9006;
+    private final int POPULATE_WITH_CONTENT_2    = 9007;
+    private final int POPULATE_WITH_CONTENT_3    = 9008;
 
     private Socket socket;
     private OutputStream outputstream;
     private Handler handler;
     private int port;
-    private long my_id;
-    private String host, my_name;
+    private long id;
+    private String ip, name;
 
-    public Client(Handler handler, int port, String my_name) {
+    public Client(Handler handler, int port, String name) {
         this.handler = handler;
         this.port = port;
-        this.my_name = my_name;
+        this.name = name;
     }
 
-    public long getId() {
-        return my_id;
-    }
-
-    public String getName() {
-        return my_name;
-    }
-
-    public void connect(String host) {
-        this.host = host;
+    public void connect(String ip) {
+        this.ip = ip;
     }
 
     public void shutdown() {
@@ -133,17 +136,6 @@ public class Client implements Runnable {
         }
     }
 
-    public void send(byte[] buffer, int offset, int size) {
-
-        send(size);
-
-        try {
-            outputstream.write(buffer, offset, size);
-        } catch (IOException e){
-            sendMessage(LOG_MESSAGE, e.toString());
-        }
-    }
-
     private void sendMessage(int what, Object obj) {
         Message msg;
 
@@ -151,8 +143,16 @@ public class Client implements Runnable {
         msg.sendToTarget();
     }
 
-    private void onNeedData(byte[] buffer, int[] index, int[] buffer_size, boolean[] need_data, ArrayList<Integer> order, int[] next) {
+    //   |<--------------------------------- BUFFER_SIZE ------------------------------->|
+    //             |<------------ on hand ----------->|<---------- buffer size --------->|
+    //   +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+    //   |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 |
+    //   +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+    //                ^                                  ^
+    //                |                                  |
+    //             index[0]                           index[1]
 
+    private void onNeedData(byte[] buffer, int[] index, int[] buffer_size, boolean[] need_data, ArrayList<Integer> order, int[] next) {
         int i, k, n;
 
         n = index[1] - index[0];
@@ -181,6 +181,7 @@ public class Client implements Runnable {
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
+
             case ATTRIBUTES:
                 order.add(GET_INTEGER);
                 order.add(GET_STRING);
@@ -189,42 +190,62 @@ public class Client implements Runnable {
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
+
             case LEAVE:
                 order.add(GET_LONG);
                 order.add(DEPOPULATE_BY_DEVICE);
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
-            case CONFIRM:
+
+            case CONNECTED:
                 order.add(GET_LONG);
-                order.add(ID_RECEIVED);
+                order.add(CONNECTED);
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
+
             case REPLY_DEVICE:
-                order.add(GET_INTEGER);
-                order.add(GET_STRING);
-                order.add(GET_LONG);
                 order.add(POPULATE_WITH_DEVICE);
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
-            case REPLY_DEVICES:
-                order.add(POPULATE_WITH_DEVICES);
-                order.add(GET_INTEGER);
-                order.add(SET_ORDER);
-                break;
+
             case REPLY_DRIVE:
                 order.add(POPULATE_WITH_DRIVES);
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
-            case REQUEST_DRIVE:
-                order.add(GET_LONG);
-                order.add(ENUMERATE_DRIVES);
+
+            case REPLY_DIRECTORY:
+                order.add(SET_ORDER);
+                break;
+
+            case REPLY_FILE:
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
+
+            case REQUEST_DRIVE:
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
+            case REQUEST_DIRECTORY:
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
+            case REQUEST_FILE:
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
+            case REQUEST_CONTENT:
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
             default:
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
@@ -238,7 +259,7 @@ public class Client implements Runnable {
         int avail_size, req_size;
 
         avail_size = index[1] - index[0];
-        req_size = Integer.SIZE / 8;
+        req_size = Integer.BYTES;
 
         if (avail_size < req_size) {
             order.add(0, GET_INTEGER);
@@ -259,7 +280,7 @@ public class Client implements Runnable {
         int avail_size, req_size;
 
         avail_size = index[1] - index[0];
-        req_size = Long.SIZE / 8;
+        req_size = Long.BYTES;
 
         if (avail_size < req_size) {
             order.add(0, GET_LONG);
@@ -276,11 +297,10 @@ public class Client implements Runnable {
     }
 
     private void onGetString(byte[] buffer, int[] index, ArrayList<Integer> order, int[] next, int len, String[] str) {
-
         int avail_size, req_size;
 
         avail_size = index[1] - index[0];
-        req_size = len * Short.SIZE / 8;
+        req_size = len * Short.BYTES;
 
         if (avail_size < req_size) {
             order.add(0, GET_STRING);
@@ -294,25 +314,19 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
-    private void onCollectString(ArrayList<Integer> order, int[] next, ArrayList<String> array, String str) {
-        array.add(str);
-        next[0] = order.remove(0);
-    }
-
     private void onCollectAttribute(ArrayList<Integer> order, int[] next, ArrayList<Attribute> array, String str, long value) {
         array.add(new Attribute(str, value));
         next[0] = order.remove(0);
     }
 
-    private void onRunOnce(ArrayList<Integer> order, int[] next) {
+    private void onFirstRun(ArrayList<Integer> order, int[] next) {
         send(JOIN);
-        send(my_name);
+        send(name);
         next[0] = order.remove(0);
     }
 
-    private void onIdReceived(ArrayList<Integer> order, int[] next, long value) {
-        my_id = value;
-        sendMessage(Client.LOG_MESSAGE, String.format("The client thread 0x%x has started.", my_id));
+    private void onConnected(ArrayList<Integer> order, int[] next, long value) {
+        id = value;
         next[0] = order.remove(0);
     }
 
@@ -321,22 +335,8 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
-    private void onPopulateWithDevice(ArrayList<Integer> order, int[] next, String str, long value) {
-        Attribute attribute = new Attribute(str, value);
-        sendMessage(POPULATE_WITH_DEVICE, attribute);
-        next[0] = order.remove(0);
-    }
-
-    private void onPopulateWithDevices(ArrayList<Integer> order, int[] next, ArrayList<Attribute> array) {
-        sendMessage(POPULATE_WITH_DEVICES, array);
-        next[0] = order.remove(0);
-    }
-
-    private void onPopulateWithDrives(ArrayList<Integer> order, int[] next, ArrayList<String> array) {
-        next[0] = order.remove(0);
-    }
-
-    private void onEnumerateDrives(ArrayList<Integer> order, int[] next, long value) {
+    private void onPopulateWithDevice(ArrayList<Integer> order, int[] next, ArrayList<Attribute> array) {
+        sendMessage(POPULATE_WITH_DEVICE, array);
         next[0] = order.remove(0);
     }
 
@@ -349,34 +349,34 @@ public class Client implements Runnable {
         int[] buffer_size = new int[1];
         int[] index = new int[2];
         int[] next = new int[1];
+        int[] ivalue = new int[1];
+        long[] lvalue = new long[1];
+        String[] str = new String[1];
         byte[] buffer = new byte[BUFFER_SIZE];
         boolean[] need_data = new boolean[1];
         int count;
 
-        int[] ivalue = new int[1];
-        long[] lvalue = new long[1];
-        String[] str = new String[1];
-
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
 
+        sendMessage(LOG_MESSAGE, "The client thread has started.");
         sendMessage(CONNECTING, null);
 
         try {
-
-            socket = new Socket(host, port);
-
-            inputstream = socket.getInputStream();
-            outputstream = socket.getOutputStream();
-
-            sendMessage(RUNNING, null);
 
             buffer_size[0] = BUFFER_SIZE;
             count = 0;
             index[0] = index[1] = 0;
             need_data[0] = false;
-            next[0] = RUN_ONCE;
+            next[0] = FIRST_RUN;
             order.add(GET_INTEGER);
             order.add(SET_ORDER);
+
+            socket = new Socket(ip, port);
+
+            inputstream = socket.getInputStream();
+            outputstream = socket.getOutputStream();
+
+            sendMessage(RUNNING, null);
 
             while (true) {
 
@@ -406,15 +406,11 @@ public class Client implements Runnable {
                     case GET_INTEGER:					onGetInteger(buffer, index, order, next, ivalue);				break;
                     case GET_LONG:					    onGetLong(buffer, index, order, next, lvalue);				    break;
                     case GET_STRING:					onGetString(buffer, index, order, next, ivalue[0], str);		break;
-                    case COLLECT_STRING:				onCollectString(order, next, array_s, str[0]);					break;
-                    case COLLECT_ATTRIBUTE:				onCollectAttribute(order, next, array_a, str[0], lvalue[0]);	break;
-                    case RUN_ONCE:						onRunOnce(order, next);											break;
-                    case ID_RECEIVED:					onIdReceived(order, next, lvalue[0]);							break;
+                    case COLLECT_ATTRIBUTE:				onCollectAttribute(order, next, array_a, str[0], lvalue[0]);    break;
+                    case FIRST_RUN:						onFirstRun(order, next);		                                break;
+                    case CONNECTED:                     onConnected(order, next, lvalue[0]);                            break;
                     case DEPOPULATE_BY_DEVICE:			onDepopulateByDevice(order, next, lvalue[0]);					break;
-                    case POPULATE_WITH_DEVICE:			onPopulateWithDevice(order, next, str[0], lvalue[0]);			break;
-                    case POPULATE_WITH_DEVICES:			onPopulateWithDevices(order, next, array_a);					break;
-                    case POPULATE_WITH_DRIVES:			onPopulateWithDrives(order, next, array_s);						break;
-                    case ENUMERATE_DRIVES:				onEnumerateDrives(order, next, lvalue[0]);						break;
+                    case POPULATE_WITH_DEVICE:			onPopulateWithDevice(order, next, array_a);						break;
                 }
             }
 
@@ -425,5 +421,7 @@ public class Client implements Runnable {
         if(socket != null) shutdown();
 
         sendMessage(SHUTTING_DOWN, this);
+        sendMessage(LOG_MESSAGE, "The client thread has exited.");
+
     }
 }
