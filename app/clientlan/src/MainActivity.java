622c5653d3e5c5e9b818557c986a723b57e06dfc
imbakan <131257516+imbakan@users.noreply.github.com>
2025-06-23 08:25:00 +0800

    initial commit

--- app/clientlan/src/MainActivity.java
+++ app/clientlan/src/MainActivity.java
@@ -1,17 +1,6 @@
+package balikbayan.box.client_lan;
 
-// client-server model
-// client side
-// LAN
-// Android 13 Api 33
-
-package balikbayan.box.clientlan;
-
-import androidx.activity.OnBackPressedCallback;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,56 +10,72 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TreeViewEventListener {
+public class MainActivity extends AppCompatActivity {
 
-    private final int port = 27015;
-    private final String default_ip = "192.168.1.1";
+    private final int port = 54105;
+    private final String default_ip = "192.168.1.5";
 
-    private RecyclerView recyclerView1, recyclerView2;
-    private TextView textView1;
-    private EditText editText1, editText2;
-
+    private Toolbar toolbar;
+    private EditText editText1;
+    private ViewPager2 viewPager;
     private Client client;
     private Handler handler;
-    private boolean running, item_selected;
-
-    OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(false) {
-        @Override
-        public void handleOnBackPressed() {
-            Toast.makeText(getApplicationContext(), "TUMATAKBO PA ANG CLIENT", Toast.LENGTH_SHORT).show();
-        }
-    };
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String str = getResources().getString(R.string.app_name);
-        setTitle(str + " - offline");
+        getWindow().setStatusBarColor(Color.BLACK);
 
-        item_selected = running = false;
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("OFFLINE");
+        setSupportActionBar(toolbar);
 
-        editText1 = findViewById(R.id.editText1);
+        editText1 = findViewById(R.id.editText3);
         editText1.setKeyListener(null);
 
-        editText2 = findViewById(R.id.editText2);
-        editText2.setKeyListener(null);
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
 
-        textView1 = findViewById(R.id.textView1);
+            }
+        });
 
-        recyclerView1 = findViewById(R.id.recyclerView1);
-        recyclerView1.setLayoutManager(new LinearLayoutManager(this));
-        TreeViewAdapter adapter1 = new TreeViewAdapter(this, (TreeViewEventListener) this);
-        recyclerView1.setAdapter(adapter1);
+        Fragment1 fragment1 =  new Fragment1(this, new Fragment1.OnEventListener() {
+            @Override
+            public void onItemClick() {
+                onToolsDebug2();
+            }
 
-        getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback);
+            @Override
+            public void onItemSelected() {
+
+            }
+
+            @Override
+            public void onItemUnselected() {
+
+            }
+        });
+
+        Fragment2 fragment2 =  new Fragment2();
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        adapter.add(fragment1);
+        adapter.add(fragment2);
+        viewPager.setAdapter(adapter);
 
         //  +------------------------------------------------------------------------+
         //  |                          handler message                               |
@@ -78,24 +83,24 @@ public class MainActivity extends AppCompatActivity implements TreeViewEventList
 
         handler = new Handler(Looper.getMainLooper()) {
             @Override
-            public void handleMessage(Message msg) {
+            public void handleMessage(@NonNull Message msg) {
 
                 switch (msg.what){
-                    case ConnectDialog.CONNECT:                 onConnect(msg);                 break;
                     case Client.LOG_MESSAGE:                    onLogMessage(msg);              break;
                     case Client.CONNECTING:                     onConnecting(msg);              break;
                     case Client.RUNNING:                        onRunning(msg);                 break;
                     case Client.SHUTTING_DOWN:                  onShuttingDown(msg);            break;
-                    case Client.POPULATE_WITH_DEVICES:          onPopulateWithDevices(msg);     break;
-                    case Client.POPULATE_WITH_DEVICE:           onPopulateWithDevice(msg);      break;
                     case Client.DEPOPULATE_BY_DEVICE:           onDepopulateByDevice(msg);      break;
+                    case Client.POPULATE_WITH_DEVICE:           onPopulateWithDevice(msg);      break;
                     default:
                         super.handleMessage(msg);
                 }
             }
         };
 
-        initialize();
+        // ITO AY ILALAGAY SA INITIALIZE
+        String name = Build.MANUFACTURER.toString().toUpperCase() + " " + Build.MODEL.toString();
+        client = new Client(handler, port, name);
     }
 
     @Override
@@ -109,13 +114,13 @@ public class MainActivity extends AppCompatActivity implements TreeViewEventList
 
         menu.findItem(R.id.mnuConnect).setEnabled(!running);
         menu.findItem(R.id.mnuDisconnect).setEnabled(running);
+        menu.findItem(R.id.mnuDownload).setEnabled(false);
 
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         int id = item.getItemId();
 
         if(id == R.id.mnuConnect)
@@ -123,7 +128,11 @@ public class MainActivity extends AppCompatActivity implements TreeViewEventList
         else if(id == R.id.mnuDisconnect)
             onClientDisconnect();
         else if(id == R.id.mnuDownload)
-            onToolsDownload();
+            onFileDownload();
+        else if(id == R.id.mnuDebug1)
+            onToolsDebug1();
+        else if(id == R.id.mnuDebug2)
+            onToolsDebug2();
         else
             return super.onOptionsItemSelected(item);
 
@@ -131,7 +140,15 @@ public class MainActivity extends AppCompatActivity implements TreeViewEventList
     }
 
     private void onClientConnect() {
-        ConnectDialog dlg = new ConnectDialog(handler, default_ip);
+
+        ConnectDialog dlg = new ConnectDialog(default_ip, new ConnectDialog.OnClickListener() {
+            @Override
+            public void onClick(String str) {
+                client.connect(str);
+                new Thread(client).start();
+            }
+        });
+
         dlg.show(getSupportFragmentManager(), "connect dialog");
     }
 
@@ -139,131 +156,77 @@ public class MainActivity extends AppCompatActivity implements TreeViewEventList
         client.shutdown();
     }
 
-    private void onToolsDownload() {
+    private void onFileDownload() {
+        ViewPagerAdapter adapter = (ViewPagerAdapter) viewPager.getAdapter();
+        Fragment1 fragment = (Fragment1) adapter.getItem(0);
     }
 
-    private void initialize() {
+    private void onToolsDebug1() {
+        ViewPagerAdapter adapter = (ViewPagerAdapter) viewPager.getAdapter();
+        Fragment1 fragment = (Fragment1) adapter.getItem(0);
+        ListViewItem1 item = fragment.getItem();
+        ArrayList<String> array = new ArrayList<>();
 
-        String name = Build.MANUFACTURER.toString().toUpperCase() + " " + Build.MODEL.toString();
-        client = new Client(handler, port, name);
+        array.add("Isa");
+        array.add("Dalawa");
+        array.add("Tatlo");
+        array.add("apat");
+        array.add("lima");
+
+        fragment.expand(item, array, R.raw.drive1);
     }
 
-    private void sendMessage(int what, Object obj) {
-        Message msg;
-
-        msg = handler.obtainMessage(what, obj);
-        msg.sendToTarget();
+    private void onToolsDebug2() {
     }
 
     private void onLogMessage(Message msg) {
         String str1, str2, str3;
 
-        str1 = editText2.getText().toString();
+        str1 = editText1.getText().toString();
         str2 = (String) msg.obj;
         str3 = str1 + "\n" + str2;
-        editText2.setText(str3);
-    }
-
-    private void onConnect(Message msg) {
-        String host = (String) msg.obj;
-        client.connect(host);
-        new Thread(client).start();
+        editText1.setText(str3);
+        editText1.setSelection(editText1.length());
     }
 
     private void onConnecting(Message msg) {
-
+        toolbar.setTitle("CONNECTING ...");
         running = true;
-
-        String str = getResources().getString(R.string.app_name);
-        setTitle(str + " - Connecting");
     }
 
     private void onRunning(Message msg) {
-
+        toolbar.setTitle("ONLINE");
         running = true;
-
-        String str = getResources().getString(R.string.app_name);
-        setTitle(str + " - Online");
-
-        onBackPressedCallback.setEnabled(running);
     }
 
     private void onShuttingDown(Message msg) {
-        Client client = (Client)msg.obj;
-
+        toolbar.setTitle("OFFLINE");
         running = false;
 
-        String str = getResources().getString(R.string.app_name);
-        setTitle(str + " - Offline");
-
-        TreeViewAdapter adapter =  (TreeViewAdapter) recyclerView1.getAdapter();
-        int n = adapter.getItemCount();
-        adapter.clear();
-        adapter.notifyItemRangeRemoved(0, n);
-
-        onBackPressedCallback.setEnabled(running);
-
-        sendMessage(Client.LOG_MESSAGE, String.format("The client thread 0x%x has exited.", client.getId()));
-    }
-
-    private void onPopulateWithDevices(Message msg) {
-        ArrayList<Attribute> array = (ArrayList<Attribute>)msg.obj;
-        TreeViewAdapter adapter;
-        Attribute attribute;
-        int i, n;
-
-        adapter =  (TreeViewAdapter) recyclerView1.getAdapter();
-
-        n = array.size();
-
-        for(i=0; i<n; i++) {
-
-            attribute = array.remove(0);
-            adapter.add(new TreeViewItem(null, attribute.getString(), attribute.getValue(), R.raw.device1, 0, 0));
-        }
-
-        adapter.notifyItemRangeInserted(0, n);
-        textView1.setText(String.format(Locale.US, "%d devices", n));
-    }
-
-    private void onPopulateWithDevice(Message msg) {
-        Attribute attribute = (Attribute)msg.obj;
-        TreeViewAdapter adapter;
-        int k;
-
-        adapter =  (TreeViewAdapter) recyclerView1.getAdapter();
-        adapter.add(new TreeViewItem(null, attribute.getString(), attribute.getValue(), R.raw.device1, 0, 0));
-        k = adapter.getItemCount() - 1;
-        adapter.notifyItemInserted(k);
+        ViewPagerAdapter adapter = (ViewPagerAdapter) viewPager.getAdapter();
+        Fragment1 fragment = (Fragment1) adapter.getItem(0);
+        fragment.clear();
     }
 
     private void onDepopulateByDevice(Message msg) {
-        long value = (long)msg.obj;
-        TreeViewAdapter adapter;
-        TreeViewItem item = null;
-        int i, n;
+        long id = (long) msg.obj;
 
-        adapter =  (TreeViewAdapter) recyclerView1.getAdapter();
-        n = adapter.getItemCount();
+        ViewPagerAdapter adapter;
+        Fragment1 fragment;
 
-        for (i=0; i<n; i++) {
-            item = adapter.getItem(i);
-            if (item.getHierarchy() == 0 && item.getValue() == value)
-                break;
-        }
-
-        adapter.remove(item);
-        adapter.notifyItemRemoved(i);
-
+        adapter = (ViewPagerAdapter) viewPager.getAdapter();
+        fragment = (Fragment1) adapter.getItem(0);
+        fragment.removeRoot(id);
     }
 
-    @Override
-    public void onItemClick(TreeViewItem item) {
+    private void onPopulateWithDevice(Message msg) {
+        ArrayList<Attribute> array = (ArrayList<Attribute>) msg.obj;
+        ViewPagerAdapter adapter;
+        Fragment1 fragment;
 
+        adapter = (ViewPagerAdapter) viewPager.getAdapter();
+        fragment = (Fragment1) adapter.getItem(0);
+        fragment.addRoot(array);
     }
 
-    @Override
-    public void onItemLongClick() {
-
-    }
 }
