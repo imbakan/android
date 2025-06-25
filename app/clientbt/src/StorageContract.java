ddf8cfb24dcb095a67fbc6c40b34875a78292911
imbakan <131257516+imbakan@users.noreply.github.com>
2025-06-26 06:56:47 +0800

    initial commit

---
+++ app/clientbt/src/StorageContract.java
@@ -0,0 +1,31 @@
+package balikbayan.box.client_bt;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
+
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class StorageContract extends ActivityResultContract<Void, Boolean> {
+
+    @NonNull
+    @Override
+    public Intent createIntent(@NonNull Context context, Void unused) {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+        intent.setData(Uri.parse("package:" + context.getApplicationContext().getPackageName()));
+        if (!(context instanceof Activity)) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        return intent;
+    }
+
+    @Override
+    public Boolean parseResult(int i, @Nullable Intent intent) {
+        return Environment.isExternalStorageManager();
+    }
+}
