cca30fa2360e8dbe6c2d9e1b2f2d2624ac108c97
imbakan <131257516+imbakan@users.noreply.github.com>
2025-06-26 06:52:43 +0800

    initial commit

---
+++ app/clientbt/src/ListViewItem1.java
@@ -0,0 +1,54 @@
+package balikbayan.box.client_bt;
+
+public class ListViewItem1 {
+
+    private String str;
+    private int icon, depth, indent;
+    private long id;
+    private boolean collapse;
+
+    public ListViewItem1(String str, long id, int icon, int depth, int indent) {
+        this.str = str;
+        this.id = id;
+        this.icon = icon;
+        this.depth = depth;
+        this.indent = indent;
+        collapse = true;
+    }
+
+    public String getString() {
+        return str;
+    }
+
+    public void setString(String str) {
+        this.str = str;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int getIndent() {
+        return indent;
+    }
+
+    public void setCollapse(boolean collapse) {
+        this.collapse = collapse;
+    }
+
+    public boolean isCollapse() {
+        return collapse;
+    }
+}
