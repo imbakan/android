6918a0277fb5dc282779a75cb3ad49759afa2e85
imbakan <131257516+imbakan@users.noreply.github.com>
2025-06-26 06:49:25 +0800

    initial commit

---
+++ app/clientbt/src/ListViewHolder1.java
@@ -0,0 +1,36 @@
+package balikbayan.box.client_bt;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListViewHolder1 extends RecyclerView.ViewHolder {
+
+    private ImageView imageView;
+    private TextView textView;
+    private ConstraintLayout layout;
+
+    public ListViewHolder1(@NonNull View itemView) {
+        super(itemView);
+
+        textView = itemView.findViewById(R.id.textView11);
+        imageView = itemView.findViewById(R.id.imageView11);
+        layout = itemView.findViewById(R.id.layout1);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public ConstraintLayout getLayout() {
+        return layout;
+    }
+}
