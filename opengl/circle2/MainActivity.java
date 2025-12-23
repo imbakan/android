78c29954b5c24797e630cf73e2ef324f031f147f
imbakan <131257516+imbakan@users.noreply.github.com>
2025-12-23 13:39:03 +0800

    initial commit

---
+++ opengl/circle2/MainActivity.java
@@ -0,0 +1,32 @@
+package balikbayan.box.circle2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new SurfaceView(this);
+        setContentView(view);
+
+        // draw behind status bar and the navigation bar.
+        EdgeToEdge.enable(this);
+
+        // Make the system bars to disappear and reappear after a short time
+        WindowInsetsControllerCompat windowInsetsController = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+
+        // hide status bar and the navigation bar.
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+    }
+}
