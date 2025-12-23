06674c300f0bfe56243e766baf0922dec720c849
imbakan <131257516+imbakan@users.noreply.github.com>
2025-12-23 10:28:12 +0800

    initial commit

---
+++ opengl/triangle3/Renderer.java
@@ -0,0 +1,85 @@
+package balikbayan.box.triangle3;
+
+import android.content.Context;
+import android.opengl.GLES32;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Renderer  implements GLSurfaceView.Renderer {
+
+    private float[] matrix;
+    private Shader shader;
+    private Model model;
+
+    public Renderer(Context context) {
+
+        matrix = new float[16];
+
+        shader = new Shader(context);
+        model = new Model();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        String source1, source2;
+
+        source1 = "#version 320 es\n" +
+                "\n" +
+                "precision mediump float;\n" +
+                "\n" +
+                "in vec2 v_vertex;\n" +
+                "\n" +
+                "uniform mat4 m_matrix;\n" +
+                "\n" +
+                "void main()\n" +
+                "{\n" +
+                "\tgl_Position =  m_matrix * vec4(vec3(v_vertex, 0.0), 1.0);\n" +
+                "}";
+
+        source2 = "#version 320 es\n" +
+                "\n" +
+                "precision mediump float;\n" +
+                "\n" +
+                "out vec4 FragColor;\n" +
+                "\n" +
+                "void main()\n" +
+                "{\n" +
+                "\tFragColor = vec4(0.5, 0.5, 0.5, 1.0);\n" +
+                "}";
+
+        shader.create(source1, source2);
+        shader.use();
+
+        model.create(shader.getHandle());
+
+        GLES32.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+        float left, right, bottom, top, near, far;
+        float w, h;
+
+        w = 50.0f;
+        h = w * ((float) i1 / (float) i);
+
+        left = -w / 2.0f;
+        right = left + w;
+        bottom = -h / 2.0f;
+        top = bottom + h;
+        near = 0.0f;
+        far = 1.0f;
+
+        Matrix.orthoM(matrix, 0, left, right, bottom, top, near, far);
+        GLES32.glViewport(0, 0, i, i1);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT);
+        model.draw(matrix);
+    }
+}
