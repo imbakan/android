7722f3c5d11c1382769d82a213892f3badef8dda
imbakan <131257516+imbakan@users.noreply.github.com>
2025-12-23 11:43:20 +0800

    initial commit

---
+++ opengl/square1/SurfaceView.java
@@ -0,0 +1,18 @@
+package balikbayan.box.square1;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class SurfaceView extends GLSurfaceView {
+
+    private balikbayan.box.square1.Renderer renderer;
+
+    public SurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(3);
+
+        renderer = new balikbayan.box.square1.Renderer(context);
+        setRenderer(renderer);
+    }
+}
