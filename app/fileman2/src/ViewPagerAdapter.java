060ad9f893da525fa9e502a2bd4201fdb3c772c6
imbakan <131257516+imbakan@users.noreply.github.com>
2025-12-31 08:57:53 +0800

    initial commit

---
+++ app/fileman2/src/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package balikbayan.box.fileman;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    private ArrayList<Fragment> array;
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+        array = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return array.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
+
+    public void add(Fragment fragment) {
+        array.add(fragment);
+    }
+
+    public Fragment getItem(int position) {
+        return array.get(position);
+    }
+}
