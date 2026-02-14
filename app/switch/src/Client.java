8a5cd3ec0edc8ddab121ae1f1733110a3871fc73
imbakan <131257516+imbakan@users.noreply.github.com>
2026-02-14 08:51:13 +0800

    initial commit

--- app/switch/src/Client.java
+++ app/switch/src/Client.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 
 public class Client implements Runnable {
 
-    private final UUID MY_UUID = UUID.fromString("b1899020-c25d-489b-a700-42304d6adbbc");
+    private final UUID MY_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
 
     private final int BUFFER_SIZE = 1024;
 
