a2a75df3fca0c42daf188c043ed6d6b00765ef42
imbakan <131257516+imbakan@users.noreply.github.com>
2026-03-01 09:29:49 +0800

    initial commit

---
+++ app/recyclerviewsample2/src/InputDialog.java
@@ -0,0 +1,106 @@
+package balikbayan.box.recyclerviewsample2;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class InputDialog extends DialogFragment {
+
+    private AlertDialog dialog;
+    private Context context;
+    private OnClickListener listener;
+    private int resId;
+    private String str;
+
+    public InputDialog(Context context, OnClickListener listener, int resId, String str) {
+        this.context = context;
+        this.listener = listener;
+        this.resId = resId;
+        this.str = str;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = getBuilder(null);
+        dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+
+        return dialog;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+    }
+
+    private AlertDialog.Builder getBuilder(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        TextView textView = new TextView(context);
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        textView.setHeight(128);
+        textView.setPadding(0, 32, 0, 0);
+        textView.setText(resId);
+        builder.setCustomTitle(textView);
+
+        EditText editText = new EditText(context);
+        builder.setView(editText);
+
+        if (str != null)
+            editText.setText(str);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(charSequence.length() > 0);
+            }
+        });
+
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String str = editText.getText().toString();
+                listener.onClick(str);
+            }
+        });
+
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+
+        return builder;
+    }
+
+    public interface OnClickListener {
+        void onClick(String str);
+    }
+
+}
