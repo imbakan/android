e9544bfb2a1faa1caf06c7bf7e2c17a2ef8fbac5
imbakan <131257516+imbakan@users.noreply.github.com>
2026-04-13 19:16:52 +0800

    initial commit

--- app/light/src/Client.java
+++ app/light/src/Client.java
@@ -1,8 +1,13 @@
 package balikbayan.box.light;
 
+import android.app.admin.DevicePolicyManager;
 import android.bluetooth.BluetoothSocket;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 
 import java.io.IOException;
@@ -15,8 +20,8 @@ public class Client implements Runnable {
 
     private final int BUFFER_SIZE = 1024;
 
-    public static final int RUNNING         = 2002;
-    public static final int SHUTTING_DOWN   = 2003;
+    public static final int RUNNING         = 2001;
+    public static final int SHUTTING_DOWN   = 2002;
 
     private final int NEED_DATA     = 3001;
     private final int SET_ORDER     = 3002;
@@ -26,23 +31,20 @@ public class Client implements Runnable {
 
     public static final int LIGHT_ON        = 5001;
     public static final int LIGHT_OFF       = 5002;
-    public static final int CURRENT_STATE   = 5003;
 
     public static final int LIGHT_COLOR = 6001;
+    public static final int LIGHT_BRIGHTNESS = 6002;
 
     private BluetoothSocket socket;
     private Handler handler;
     private OutputStream outputstream;
+    private Context context;
     private boolean running;
-    private int state;
 
-    public Client() {
-    }
-
-    public void init(BluetoothSocket socket, Handler handler, int state) {
+    public Client(Context context, BluetoothSocket socket, Handler handler) {
+        this.context = context;
         this.socket = socket;
         this.handler = handler;
-        this.state = state;
         running = false;
     }
 
@@ -72,6 +74,20 @@ public class Client implements Runnable {
         }
     }
 
+    public void send(long value) {
+        ByteBuffer bb;
+        byte[] b;
+
+        try {
+            bb = ByteBuffer.allocate(Long.SIZE / 8);
+            bb.putLong(value);
+            b = bb.array();
+            outputstream.write(b);
+        } catch (IOException e){
+            Log.d("KLGYN", e.toString());
+        }
+    }
+
     private void sendMessage(Handler handler, int what, Object obj) {
         Message msg;
 
@@ -91,6 +107,12 @@ public class Client implements Runnable {
                 break;
             case LIGHT_OFF:
                 order.add(LIGHT_OFF);
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+            case LIGHT_BRIGHTNESS:
+                order.add(GET_INTEGER);
+                order.add(LIGHT_BRIGHTNESS);
                 order.add(GET_INTEGER);
                 order.add(SET_ORDER);
                 break;
@@ -154,20 +176,53 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
+    // ito ay para sa first time kumonect ang client sa server
+    // isend ang state ng ilaw (on/off, brightness)
+    // kapag running na ang client, ang code ay nasa sa service
     private void onFirstRun(ArrayList<Integer> order, int[] next) {
+        PowerManager manager;
+        int brightness;
+        boolean screen_on;
 
-        send(CURRENT_STATE);
-        send(state);
+        manager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        screen_on = manager.isInteractive();
+
+        if (screen_on)
+            send(LIGHT_ON);
+        else
+            send(LIGHT_OFF);
+
+        brightness = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+
+        send(LIGHT_BRIGHTNESS);
+        send(brightness);
         next[0] = order.remove(0);
     }
 
     private void onLightOn(ArrayList<Integer> order, int[] next) {
-        sendMessage(handler, LIGHT_ON, null);
+
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP,"MyApp:MyWakelockTag");
+        wakeLock.acquire();
+        wakeLock.release();
+
         next[0] = order.remove(0);
     }
 
     private void onLightOff(ArrayList<Integer> order, int[] next) {
-        sendMessage(handler, LIGHT_OFF, null);
+
+        DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        devicePolicyManager.lockNow();
+
+        next[0] = order.remove(0);
+    }
+
+    private void onLightBrightness(ArrayList<Integer> order, int[] next, int value) {
+
+        ContentResolver contentResolver = context.getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, value);
+
         next[0] = order.remove(0);
     }
 
@@ -187,6 +242,8 @@ public class Client implements Runnable {
         byte[] buffer = new byte[BUFFER_SIZE];
         boolean[] need_data = new boolean[1];
         int count;
+
+        Log.d("KLGYN", "client thread start");
 
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
 
@@ -234,13 +291,14 @@ public class Client implements Runnable {
                 //Log.d("KLGYN", String.format("%10d TASK", next[0]));
 
                 switch (next[0]) {
-                    case SET_ORDER:		onSetOrder(need_data, order, next, value[0]);					break;
-                    case NEED_DATA:		onNeedData(buffer, index, buffer_size, need_data, order, next); break;
-                    case GET_INTEGER:	onGetInteger(buffer, index, order, next, value);				break;
-                    case FIRST_RUN:		onFirstRun(order, next);									    break;
-                    case LIGHT_ON:		onLightOn(order, next);									        break;
-                    case LIGHT_OFF:		onLightOff(order, next);									    break;
-                    case LIGHT_COLOR:	onLightColor(order, next, value[0]);							break;
+                    case SET_ORDER:		    onSetOrder(need_data, order, next, value[0]);					break;
+                    case NEED_DATA:		    onNeedData(buffer, index, buffer_size, need_data, order, next); break;
+                    case GET_INTEGER:	    onGetInteger(buffer, index, order, next, value);				break;
+                    case FIRST_RUN:		    onFirstRun(order, next);									    break;
+                    case LIGHT_ON:		    onLightOn(order, next);									        break;
+                    case LIGHT_OFF:		    onLightOff(order, next);									    break;
+                    case LIGHT_COLOR:	    onLightColor(order, next, value[0]);							break;
+                    case LIGHT_BRIGHTNESS:  onLightBrightness(order, next, value[0]);						break;
                 }
             }
 
@@ -253,5 +311,8 @@ public class Client implements Runnable {
         running = false;
 
         sendMessage(handler, SHUTTING_DOWN, null);
+
+        Log.d("KLGYN", "client thread stop");
+
     }
 }
