fa4707a5098b264c52df52674fd3cdf9a59d5bc4
imbakan <131257516+imbakan@users.noreply.github.com>
2026-04-13 19:39:49 +0800

    initial commit

--- app/switch/src/MainActivity.java
+++ app/switch/src/MainActivity.java
@@ -7,10 +7,14 @@
 // App to turn on/off light remotely via bluetooth
 // A companion app : Light app
 
-package balikbayan.box.aswitch;
+package balikbayan.box.switch2;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.graphics.Color;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -18,42 +22,27 @@ import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.widget.CompoundButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Switch;
 
+import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private Switch aSwitch;
-    private RadioGroup radioGroup;
-    private RadioButton[] radioButton;
+    private Switch switch1;
     private Client client;
     private Handler handler;
-    private ArrayList<Integer> uiID = new ArrayList<>();
-    private ArrayList<String> uiText = new ArrayList<>();
-    private ArrayList<Integer> lightColor = new ArrayList<>();
-    private int[] id = new int[20];
-    private boolean access_granted, dont_send;
+    private int brightness;
+    private boolean access_granted, no_echo;
 
     // reply sa requestPermissionBTConnect
     ActivityResultLauncher<String> launcher1 = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -86,108 +75,33 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        aSwitch = findViewById(R.id.switch1);
-
-        radioGroup = findViewById(R.id.radioGroup);
-
-        uiID.add(R.id.radioButton1);
-        uiID.add(R.id.radioButton2);
-        uiID.add(R.id.radioButton3);
-        uiID.add(R.id.radioButton4);
-        uiID.add(R.id.radioButton5);
-        uiID.add(R.id.radioButton6);
-        uiID.add(R.id.radioButton7);
-        uiID.add(R.id.radioButton8);
-        uiID.add(R.id.radioButton9);
-        uiID.add(R.id.radioButton10);
-
-        uiID.add(R.id.radioButton11);
-        uiID.add(R.id.radioButton12);
-        uiID.add(R.id.radioButton13);
-        uiID.add(R.id.radioButton14);
-        uiID.add(R.id.radioButton15);
-        uiID.add(R.id.radioButton16);
-        uiID.add(R.id.radioButton17);
-        uiID.add(R.id.radioButton18);
-        uiID.add(R.id.radioButton19);
-        uiID.add(R.id.radioButton20);
-
-        uiText.add("Black Light Fluorescent");  lightColor.add(Color.rgb(167, 0, 255));
-        uiText.add("Blue");                     lightColor.add(Color.rgb(0, 0, 255));
-        uiText.add("Brown");                    lightColor.add(Color.rgb(150, 75, 0));
-        uiText.add("Bulb (100 W)");             lightColor.add(Color.rgb(255, 214, 170));
-        uiText.add("Bulb (40 W)");              lightColor.add(Color.rgb(255, 197, 143));
-        uiText.add("Bulb");                     lightColor.add(Color.rgb(255, 194, 0));
-        uiText.add("Candle");                   lightColor.add(Color.rgb(255, 147, 41));
-        uiText.add("Cyan");                     lightColor.add(Color.rgb(0, 255, 255));
-        uiText.add("Green");                    lightColor.add(Color.rgb(0, 255, 0));
-        uiText.add("Grey");                     lightColor.add(Color.rgb(128, 128, 128));
-
-        uiText.add("Indigo");                       lightColor.add(Color.rgb(75, 0, 130));
-        uiText.add("Magenta");                      lightColor.add(Color.rgb(255, 0, 255));
-        uiText.add("Orange");                       lightColor.add(Color.rgb(255, 165, 0));
-        uiText.add("Red");                          lightColor.add(Color.rgb(255, 0, 0));
-        uiText.add("Sodium Lamp");                  lightColor.add(Color.rgb(255, 209, 178));
-        uiText.add("Sodium Lamp (High Pressure)");  lightColor.add(Color.rgb(255, 183, 76));
-        uiText.add("Violet");                       lightColor.add(Color.rgb(127, 0, 255));
-        uiText.add("White");                        lightColor.add(Color.rgb(255, 255, 255));
-        uiText.add("Yellow");                       lightColor.add(Color.rgb(255, 255, 0));
-        uiText.add("Neon");                         lightColor.add(Color.rgb(255, 61, 0));
-
-        radioButton = new RadioButton[20];
-
-        int i;
-        for(i=0; i<20; i++) {
-            radioButton[i] = findViewById(uiID.get(i));
-            radioButton[i].setText(uiText.get(i));
-        }
-
-        aSwitch.setEnabled(false);
-        setRadioGroupButtonEnabled(false);
+        switch1 = findViewById(R.id.switch1);
+        switch1.setEnabled(false);
 
         //  +------------------------------------------------------------------------+
-        //  |                            light switch                                |
+        //  |                               switch                                   |
         //  +------------------------------------------------------------------------+
 
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
 
                 if (b)
-                    setRadioGroupButtonEnabled(true);
+                    compoundButton.setText(R.string.switch_name_1);
                 else
-                    setRadioGroupButtonEnabled(false);
+                    compoundButton.setText(R.string.switch_name_2);
 
-                // para lang iswitch ang switch na 'to
-                // hindi i on/off ang light
-                if (dont_send) {
-                    dont_send = false;
+                // ito ay para di ma-execute ang mga nasa baba ng if statement
+                // ito ay para lang baguhin ang display ng swtch
+                if (no_echo) {
+                    no_echo = false;
                     return;
                 }
 
-                // turn on/off light
-                if (b)
-                    onToolsTurnON();
-                else
-                    onToolsTurnOFF();
-            }
-        });
+                //Log.d("KLGYN", String.format("%s", b?"ON":"OFF"));
 
-
-        //  +------------------------------------------------------------------------+
-        //  |                            light color                                 |
-        //  +------------------------------------------------------------------------+
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull RadioGroup radioGroup, int i) {
-                RadioButton checkedRadioButton = findViewById(i);
-                int index = uiID.indexOf(i);
-
-                //Log.d("KLGYN", String.format("%10d", index));
-
-                client.send(Client.LIGHT_COLOR);
-                client.send(lightColor.get(index));
+                int state = b ? Client.LIGHT_ON : Client.LIGHT_OFF;
+                client.send(state);
             }
         });
 
@@ -200,10 +114,12 @@ public class MainActivity extends AppCompatActivity {
             public void handleMessage(Message msg) {
 
                 switch (msg.what){
-                    case Client.CONNECTING:     onClientConnecting(msg);    break;
-                    case Client.RUNNING:        onClientRunning(msg);       break;
-                    case Client.SHUTTING_DOWN:  onClientShuttingDown(msg);  break;
-                    case Client.CURRENT_STATE:  onCurrentSate(msg);         break;
+                    case Client.CONNECTING:         onClientConnecting(msg);    break;
+                    case Client.RUNNING:            onClientRunning(msg);       break;
+                    case Client.SHUTTING_DOWN:      onClientShuttingDown(msg);  break;
+                    case Client.LIGHT_BRIGHTNESS:   onLightBrightness(msg);     break;
+                    case Client.LIGHT_ON:           onLightOn(msg);             break;
+                    case Client.LIGHT_OFF:          onLightOff(msg);            break;
                     default:
                         super.handleMessage(msg);
                 }
@@ -214,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         //  |                            permission                                  |
         //  +------------------------------------------------------------------------+
 
-        access_granted = dont_send = false;
+        access_granted = no_echo = false;
 
         requestPermissionBTConnect();
 
@@ -235,19 +151,24 @@ public class MainActivity extends AppCompatActivity {
 
         menu.findItem(R.id.mnuConnect).setEnabled(access_granted && !running);
         menu.findItem(R.id.mnuDisconnect).setEnabled(access_granted && running);
+        menu.findItem(R.id.mnuBrightness).setEnabled(access_granted && running);
+        menu.findItem(R.id.mnuLight).setEnabled(access_granted && running);
 
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         int id = item.getItemId();
 
         if (id == R.id.mnuConnect)
             onClientConnect();
         else if (id == R.id.mnuDisconnect)
             onClientDisconnect();
+        else if (id == R.id.mnuBrightness)
+            onToolsBrightness();
+        else if (id == R.id.mnuLight)
+            onToolsLight();
         else
             return super.onOptionsItemSelected(item);
 
@@ -255,10 +176,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClientConnect() {
+        ConnectToDefault(this);
     }
 
     private void onClientDisconnect() {
         client.shutdown();
+    }
+
+    private void onToolsBrightness() {
+
+        BrightnessDialog dlg = new BrightnessDialog(this, new BrightnessDialog.OnChangeListener() {
+            @Override
+            public void onChange(int i) {
+                client.send(Client.LIGHT_BRIGHTNESS);
+                client.send(i);
+            }
+        }, brightness);
+
+        dlg.show(getSupportFragmentManager(), "brightness dialog");
+    }
+
+    private void onToolsLight() {
+
+        LightColorDialog dlg = new LightColorDialog(this, new LightColorDialog.OnChangeListener() {
+            @Override
+            public void onChange(int i) {
+                client.send(Client.LIGHT_COLOR);
+                client.send(i);
+            }
+        }, brightness);
+
+        dlg.show(getSupportFragmentManager(), "light dialog");
     }
 
     private void onClientConnecting(Message msg) {
@@ -267,39 +215,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void onClientRunning(Message msg) {
         toolbar.setTitle("ONLINE");
-        aSwitch.setEnabled(true);
+        switch1.setEnabled(true);
     }
 
     private void onClientShuttingDown(Message msg) {
         toolbar.setTitle("OFFLINE");
-        aSwitch.setEnabled(false);
-        setRadioGroupButtonEnabled(false);
+        switch1.setEnabled(false);
     }
 
-    private void onCurrentSate(Message msg) {
-        int li_state = (int) msg.obj;
-        int sw_pos = aSwitch.isChecked() ? Client.LIGHT_ON : Client.LIGHT_OFF;
-
-        if (li_state == sw_pos) return;
-
-        dont_send = true;
-        boolean check = !aSwitch.isChecked();
-        aSwitch.setChecked(check);
+    private void onLightBrightness(Message msg) {
+        brightness = (int)msg.obj;
     }
 
-    private void setRadioGroupButtonEnabled(boolean enabled) {
-        int i;
-
-        for (i=0; i<20; i++)
-            radioButton[i].setEnabled(enabled);
-
+    private void onLightOn(Message msg) {
+        if (!switch1.isChecked()) {
+            no_echo = true;
+            switch1.setChecked(true);
+        }
     }
 
-    private void autoConnect(Context context, String str ) {
+    private void onLightOff(Message msg) {
+        if (switch1.isChecked()) {
+            no_echo = true;
+            switch1.setChecked(false);
+        }
+    }
+
+    private void ConnectToDefault(Context context) {
 
         BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         Set<BluetoothDevice> devices = manager.getAdapter().getBondedDevices();
         BluetoothDevice target = null;
+        String str = "Lenovo";
 
         if(devices.size() > 0) {
 
@@ -309,6 +256,8 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 }
             }
+
+            Log.d("KLGYN", String.format("%-20s%-20s", target.getName(), target.getAddress()));
 
             client = new Client(handler, target);
             new Thread(client).start();
@@ -335,11 +284,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize() {
-      
         access_granted = true;
-
-       // ito ang device name kung saan nakainstall ang light app
-        autoConnect(this, "Motorola");
+        ConnectToDefault(this);
     }
 
 }
