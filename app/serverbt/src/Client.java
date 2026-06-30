85d80e0beecbd8d010875d3e849e2c49d7c3391c
imbakan <131257516+imbakan@users.noreply.github.com>
2026-06-30 08:07:25 +0800

    initial commit

--- app/serverbt/src/Client.java
+++ app/serverbt/src/Client.java
@@ -1,4 +1,21 @@
-package balikbayan.box.server_bt;
+
+// receive ng server
+//     +---------------+
+//     |   Client Id   |
+//     +---------------+
+//     |      Size     |
+//     +---------------+
+//     |      Data     |
+//     +---------------+
+
+// send ng server
+//     +---------------+
+//     |   Client Id   |
+//     +---------------+
+//     |      Data     |
+//     +---------------+
+
+package balikbayan.box.bt_server;
 
 import android.bluetooth.BluetoothSocket;
 import android.icu.text.SimpleDateFormat;
@@ -6,6 +23,7 @@ import android.icu.util.Calendar;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +36,7 @@ import java.util.Locale;
 
 public class Client implements Runnable {
 
-    public static final int LOG_MESSAGE     = 2001;
+    public static final int MESSAGE         = 2001;
     public static final int RUNNING         = 2002;
     public static final int SHUTTING_DOWN   = 2003;
 
@@ -30,62 +48,35 @@ public class Client implements Runnable {
     private final int GET_LONG       = 3004;
     private final int GET_STRING     = 3005;
 
-    public static final int ATTRIBUTES   = 4001;
-    private final int STRINGS            = 4002;
-    private final int COLLECT_STRING     = 4003;
-    private final int COLLECT_ATTRIBUTE  = 4004;
+    private final int JOIN = 5003;
+    private final int CLIENT_ID = 5004;
+    public static final int LEAVE = 5005;
 
-    private final int FIRST_RUN         = 5001;
-    public static final int CONNECTED   = 5002;
-    private final int JOIN              = 5003;
-    public static final int LEAVE       = 5004;
-    private final int FORWARD           = 5005;
-    private final int RESEND            = 5006;
-    //private final int CLIENT_RUNNING = 5007; di na 'to kailangan dito, sa c++ lang
+    public static final int ATTRIBUTE = 6001;
+    public static final int DEVICE = 6002;
 
-    private final int REQUEST_DRIVE      = 6001;
-    private final int REQUEST_DIRECTORY  = 6002;
-    private final int REQUEST_FILE       = 6003;
-    private final int REQUEST_CONTENT    = 6004;
-
-    public static final int REPLY_DEVICE = 7001;
-    private final int REPLY_DRIVE        = 7002;
-    private final int REPLY_DIRECTORY    = 7003;
-    private final int REPLY_FILE         = 7004;
-    private final int REPLY_CONTENT_1    = 7005;
-    private final int REPLY_CONTENT_2    = 7006;
-    private final int REPLY_CONTENT_3    = 7007;
-
-    private final int ENUMERATE_DRIVE       = 8001;
-    private final int ENUMERATE_DIRECTORY   = 8002;
-    private final int ENUMERATE_FILE        = 8003;
-    private final int ENUMERATE_CONTENT     = 8004;
-
-    private final int DEPOPULATE_BY_DEVICE      = 9001;
-    private final int POPULATE_WITH_DEVICE      = 9002;
-    private final int POPULATE_WITH_DRIVE       = 9003;
-    private final int POPULATE_WITH_DIRECTORY   = 9004;
-    private final int POPULATE_WITH_FILE        = 9005;
-    private final int POPULATE_WITH_CONTENT_1   = 9006;
-    private final int POPULATE_WITH_CONTENT_2   = 9007;
-    private final int POPULATE_WITH_CONTENT_3   = 9008;
+    private final int FORWARD = 7001;
+    private final int GET_DATA_SIZE = 7002;
+    private final int FORWARD_SOURCE = 7003;
 
     private BluetoothSocket socket;
     private OutputStream outputstream;
     private Handler handler;
+    private ListViewAdapter adapter;
     private String ip, name, date;
     private long id;
 
-    public Client(Handler handler, BluetoothSocket socket) {
+    public Client(Handler handler, BluetoothSocket socket, ListViewAdapter adapter) {
         this.handler = handler;
         this.socket = socket;
+        this.adapter = adapter;
     }
 
     public void shutdown() {
         try {
             socket.close();
         } catch (IOException e) {
-            sendMessage(LOG_MESSAGE, e.toString());
+            sendMessage(MESSAGE, e.toString());
         }
     }
 
@@ -100,7 +91,7 @@ public class Client implements Runnable {
             b = bb.array();
             outputstream.write(b);
         } catch (IOException e){
-            sendMessage(LOG_MESSAGE, e.toString());
+            sendMessage(MESSAGE, e.toString());
         }
     }
 
@@ -115,7 +106,7 @@ public class Client implements Runnable {
             b = bb.array();
             outputstream.write(b);
         } catch (IOException e){
-            sendMessage(LOG_MESSAGE, e.toString());
+            sendMessage(MESSAGE, e.toString());
         }
     }
 
@@ -128,7 +119,16 @@ public class Client implements Runnable {
             b = str.getBytes(StandardCharsets.UTF_16LE);
             outputstream.write(b);
         } catch (IOException e) {
-            sendMessage(LOG_MESSAGE, e.toString());
+            sendMessage(MESSAGE, e.toString());
+        }
+    }
+
+    public void send(byte[] buffer, int offset, int size) {
+
+        try {
+            outputstream.write(buffer, offset, size);
+        } catch (IOException e){
+            sendMessage(MESSAGE, e.toString());
         }
     }
 
@@ -144,11 +144,35 @@ public class Client implements Runnable {
         return id;
     }
 
-    private void sendMessage(int what, Object obj) {
-        Message msg;
+    private void onSetOrder(boolean[] need_data, ArrayList<Integer> order, int[] next, int msg) {
 
-        msg = handler.obtainMessage(what, obj);
-        msg.sendToTarget();
+        need_data[0] = false;
+
+        switch (msg) {
+            case JOIN:
+                order.add(GET_INTEGER);
+                order.add(GET_STRING);
+                order.add(RUNNING);
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
+            case FORWARD:
+                order.add(GET_LONG);
+                order.add(GET_LONG);
+                order.add(GET_DATA_SIZE);
+                order.add(FORWARD_SOURCE);
+                order.add(FORWARD);
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+                break;
+
+            default:
+                order.add(GET_INTEGER);
+                order.add(SET_ORDER);
+        }
+
+        next[0] = order.remove(0);
     }
 
     //   |<--------------------------------- BUFFER_SIZE ------------------------------->|
@@ -177,26 +201,6 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
-    private void onSetOrder(boolean[] need_data, ArrayList<Integer> order, int[] next, int msg) {
-
-        need_data[0] = false;
-
-        switch (msg) {
-            case JOIN:
-                order.add(GET_INTEGER);
-                order.add(GET_STRING);
-                order.add(RUNNING);
-                order.add(GET_INTEGER);
-                order.add(SET_ORDER);
-                break;
-            default:
-                order.add(GET_INTEGER);
-                order.add(SET_ORDER);
-        }
-
-        next[0] = order.remove(0);
-    }
-
     private void onGetInteger(byte[] buffer, int[] index, ArrayList<Integer> order, int[] next, int[] value) {
         ByteBuffer bb;
         int avail_size, req_size;
@@ -218,7 +222,7 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
-    private void onGetLong(byte[] buffer, int[] index, ArrayList<Integer> order, int[] next, long[] value) {
+    private void onGetLong(byte[] buffer, int[] index, ArrayList<Integer> order, int[] next, ArrayList<Long> array) {
         ByteBuffer bb;
         int avail_size, req_size;
 
@@ -233,7 +237,8 @@ public class Client implements Runnable {
 
         bb = ByteBuffer.wrap(buffer, index[0], req_size);
         bb.order(ByteOrder.LITTLE_ENDIAN);
-        value[0] = bb.getLong();
+        //value[0] = bb.getLong();
+        array.add(bb.getLong());
 
         index[0] += req_size;
         next[0] = order.remove(0);
@@ -264,6 +269,8 @@ public class Client implements Runnable {
 
         name = str;
 
+        //Log.d("KLGYN", String.format("client on running %s", name));
+
         c = Calendar.getInstance();
         now = c.getTimeInMillis();
         sdf = new SimpleDateFormat("MMM d, yyyy h:mm a", Locale.US);
@@ -274,24 +281,111 @@ public class Client implements Runnable {
         next[0] = order.remove(0);
     }
 
+    private void onGetDataSize(ArrayList<Integer> order, int[] next, ArrayList<Long> array, long[] size, Client[] client) {
+        Client item;
+        long id;
+        int i, n;
+
+        id = array.remove(0);
+        size[0] = array.remove(0);
+
+        //Log.d("KLGYN", String.format("client forward %d %d", id ,size[o));
+
+        // kunin ang destination client base sa id number
+        n = adapter.getItemCount();
+
+        Log.d("KLGYN", String.format("list view count %d", n));
+
+        for (i=0; i<n; i++) {
+
+            item = adapter.getItem(i);
+
+            Log.d("KLGYN", String.format("list view %s %d", item.getName(), item.getId()));
+
+            if (id == item.getId()) {
+                client[0] = item;
+                break;
+            }
+        }
+
+        next[0] = order.remove(0);
+    }
+
+    // iforward ang data na galing sa client
+    private void onForward(byte[] buffer, int[] index, int[] buffer_size, boolean[] need_data, ArrayList<Integer> order, int[] next, Client[] client, long[] data_size) {
+        long avail_size;
+
+        avail_size = index[1] - index[0];
+
+        if (avail_size < data_size[0]) {
+
+            if (avail_size > 0)
+                client[0].send(buffer, index[0], (int) avail_size);
+            index[0] = index[1] = 0;
+            buffer_size[0] = BUFFER_SIZE;
+            data_size[0] -= avail_size;
+            need_data[0] = true;
+
+        } else {
+
+            client[0].send(buffer, index[0], (int) data_size[0]);
+            index[0] += (int) data_size[0];
+            next[0] = order.remove(0);
+        }
+    }
+
+    private void onForwardSource(ArrayList<Integer> order, int[] next, Client[] client) {
+
+        Log.d("KLGYN", String.format("on forward source client id %d", id));
+
+        client[0].send(CLIENT_ID);
+        client[0].send(id);
+
+        next[0] = order.remove(0);
+    }
+
+    private void sendMessage(int what, Object obj) {
+        Message msg;
+
+        msg = handler.obtainMessage(what, obj);
+        msg.sendToTarget();
+    }
+
+    private void printOutput(int cmd) {
+        switch (cmd) {
+            case SET_ORDER:						Log.d("KLGYN", "SET_ORDER"); break;
+            case NEED_DATA:						Log.d("KLGYN", "NEED_DATA"); break;
+            case GET_INTEGER:					Log.d("KLGYN", "GET_INTEGER"); break;
+            case GET_LONG:					    Log.d("KLGYN", "GET_LONG"); break;
+            case GET_STRING:					Log.d("KLGYN", "GET_STRING"); break;
+            case GET_DATA_SIZE:					Log.d("KLGYN", "GET_DATA_SIZE"); break;
+            case FORWARD:		                Log.d("KLGYN", "FORWARD"); break;
+            case FORWARD_SOURCE:		        Log.d("KLGYN", "FORWARD_SOURCE"); break;
+            case RUNNING:				        Log.d("KLGYN", "RUNNING"); break;
+            default:                            Log.d("KLGYN", "**************");
+        }
+    }
+
     @Override
     public void run() {
         InputStream inputstream;
         ArrayList<Integer> order = new ArrayList<>();
+        ArrayList<Long> array_l = new ArrayList<>();
         ArrayList<String> array_s = new ArrayList<>();
+        Client[] client = new Client[1];
         int[] buffer_size = new int[1];
         int[] index = new int[2];
         int[] next = new int[1];
         int[] ivalue = new int[1];
-        long[] lvalue = new long[1];
+        long[] size = new long[1];
         String[] str = new String[1];
         byte[] buffer = new byte[BUFFER_SIZE];
         boolean[] need_data = new boolean[1];
         int count;
 
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+        Log.d("KLGYN", "client thread has started.");
 
-        sendMessage(LOG_MESSAGE, "Client thread has started.");
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
 
         id = SystemClock.elapsedRealtime();
 
@@ -323,29 +417,39 @@ public class Client implements Runnable {
 
                     count = inputstream.read(buffer, index[1], buffer_size[0]);
 
+                    //Log.d("KLGYN", String.format("%d bytes", count));
+
                     if (count <= 0) break;
 
                     index[1] += count;
                     need_data[0] = false;
                 }
 
+                //printOutput(next[0]);
+
                 switch (next[0]) {
-                    case SET_ORDER:						onSetOrder(need_data, order, next, ivalue[0]);					break;
-                    case NEED_DATA:						onNeedData(buffer, index, buffer_size, need_data, order, next); break;
-                    case GET_INTEGER:					onGetInteger(buffer, index, order, next, ivalue);				break;
-                    case GET_LONG:					    onGetLong(buffer, index, order, next, lvalue);				    break;
-                    case GET_STRING:					onGetString(buffer, index, order, next, ivalue[0], str);		break;
-                    case RUNNING:				        onRunning(order, next, str[0]);		                            break;
+                    case SET_ORDER:						onSetOrder(need_data, order, next, ivalue[0]);					                break;
+                    case NEED_DATA:						onNeedData(buffer, index, buffer_size, need_data, order, next);                 break;
+                    case GET_INTEGER:					onGetInteger(buffer, index, order, next, ivalue);				                break;
+                    case GET_LONG:					    onGetLong(buffer, index, order, next, array_l);				                    break;
+                    case GET_STRING:					onGetString(buffer, index, order, next, ivalue[0], str);		                break;
+                    case GET_DATA_SIZE:					onGetDataSize(order, next, array_l, size, client);		                        break;
+                    case FORWARD:		                onForward(buffer, index, buffer_size, need_data, order, next, client, size);    break;
+                    case FORWARD_SOURCE:		        onForwardSource(order, next, client);                                           break;
+                    case RUNNING:				        onRunning(order, next, str[0]);		                                            break;
                 }
             }
 
         } catch (IOException | SecurityException e) {
-            sendMessage(LOG_MESSAGE, e.toString());
+            Log.d("KLGYN", e.toString());
+            sendMessage(MESSAGE, e.toString());
         }
 
-        if(socket != null) shutdown();
+        shutdown();
 
-        sendMessage(LOG_MESSAGE, "Client thread has exited.");
         sendMessage(SHUTTING_DOWN, this);
+
+        Log.d("KLGYN", "Client thread has exited.");
+
     }
 }
