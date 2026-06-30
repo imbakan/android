1529a58e5d07f44e76000d79de2eda6c074442af
imbakan <131257516+imbakan@users.noreply.github.com>
2026-06-30 08:33:47 +0800

    initial commit

--- app/clientbt/src/FragmentLV.java
+++ app/clientbt/src/FragmentLV.java
@@ -1,19 +1,21 @@
-package balikbayan.box.client_bt;
+
+package balikbayan.box.bt_client;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
+ * Use the {@link FragmentLV#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment2 extends Fragment {
+public class FragmentLV extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class Fragment2 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Fragment2() {
+    public FragmentLV() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class Fragment2 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
+     * @return A new instance of fragment FragmentLV.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
+    public static FragmentLV newInstance(String param1, String param2) {
+        FragmentLV fragment = new FragmentLV();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,12 +55,14 @@ public class Fragment2 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        Log.d("KLGYN", "create lv");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        return inflater.inflate(R.layout.fragment_l_v, container, false);
     }
 }
